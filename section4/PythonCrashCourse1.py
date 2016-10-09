bde1c54cf125b48b748a276580876841867f6c77
Piyush Jhawar <piyushjhawar24@gmail.com>
2016-10-10 02:08:51 +0530

    PythonCrashpart1

---
+++ section4/PythonCrashCourse1.py
@@ -0,0 +1,17 @@
+1 #Integer
+1.0 #Float
+
+print (2 ** 4) #Two to the power of 4
+
+print(4%2)#Remmainder
+var = 2
+print(var)
+
+#Strings
+#'Single quotes' or "Double Quoyes"
+x = 'hello'
+print (x)
+
+num = 12
+name = 'Sam'
+print('My number is {} and my name is {}'.format(num,name))
