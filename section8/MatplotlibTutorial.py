75777b1851a8c59f806e5373b0b1fee976a54e84
piyushjhawar <piyushjhawar24@gmail.com>
2016-10-14 16:18:43 -0700

    Matplotlib Part1

---
+++ section8/MatplotlibTutorial.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+import numpy as np
+# %matplotlib inline only for jupyter notebook
+
+#2 Matplotlib Part1
+
+x = np.linspace(0,5,11)
+y = x ** 2
+
+#Functional
+plt.plot(x,y)
+
+
+
+plt.show()
